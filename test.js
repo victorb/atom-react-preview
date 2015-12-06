b2dc8b2e3f325093079d136081eaf8e81a8a47e3
Victor Bjelkholm <victor@typeform.com>
2015-12-06 18:52:57 +0100

    Adding propeditors

--- test.js
+++ test.js
@@ -8,25 +8,46 @@ export default class Name extends Component {
       backgroundColor: 'black',
       padding: '50px'
     }
+    const shared_style = {
+      margin: '10px',
+      color: '#9f9'
+    }
+    let warning = null
+    if (this.props.show_warning) {
+      const warning_style = {
+        padding: '10px',
+        color: '#f99'
+      }
+      warning = <div style={warning_style}>WARNING! Content here might or might not be true...</div>
+    }
     return <div style={style}>
-      <div>
+      {warning}
+      <div style={shared_style}>
         Hello, {this.props.name}
       </div>
-      <div>
+      <div style={shared_style}>
         You are {this.props.age} years old.
+      </div>
+      <div style={shared_style}>
+        You have {this.props.pets.length} pet...
+      </div>
+      <div style={shared_style}>
+        Is it true that you are tired? {this.props.is_tired.toString()}
       </div>
     </div>
   }
 }
 Name.propTypes = {
   name: PropTypes.string,
-  kids: PropTypes.array,
+  pets: PropTypes.array,
   age: PropTypes.number,
-  is_tired: PropTypes.bool
+  is_tired: PropTypes.bool,
+  show_warning: PropTypes.bool
 }
 Name.defaultProps = {
   name: 'Victor',
   pets: ['Gaia'],
   age: 18,
-  is_tired: false
+  is_tired: false,
+  show_warning: false
 }
